acfef1483df91ac5cde76b5f492e9103156756f4
Lin Gan <charlesgan0801@gmail.com>
2023-10-30 02:05:56 +1100

    repost

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # This is my first website
-Build based on Nuxt3.js (look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.)
+Build based on Nuxt3.js (look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/<gcwebsite>/', // baseURL: '/<repository>/'
+    buildAssetsDir: '.output', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  }
 })
