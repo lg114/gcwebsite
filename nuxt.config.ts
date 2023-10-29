f1e86a0b9a4f21bbd236f0bd49b1010dc2c6b01d
Lin Gan <charlesgan0801@gmail.com>
2023-10-30 02:13:01 +1100

    repost

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  app: {
-    baseURL: '/<gcwebsite>/', // baseURL: '/<repository>/'
-    buildAssetsDir: '.output', // don't use "_" at the begining of the folder name to avoids nojkill conflict
-  }
+  devtools: { enabled: true }
 })
